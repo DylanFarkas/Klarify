652cb7154bcc8bd29846f0c4450222199c45d480
DylanFarkas <dylan.farkas@correounivalle.edu.co>
2026-05-30 12:10:15 -0500

    feat: more landing components

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
 import { AutomatedWorkflow } from "@/components/landing/AutomatedWorkflow";
+import { BacklogCta } from "@/components/landing/BacklogCta";
 import { HeroLanding } from "@/components/landing/HeroLanding";
 import { IdeaToBacklog } from "@/components/landing/IdeaToBacklog";
 import { Navbar } from "@/components/landing/Navbar/Navbar";
+import { PricingPlans } from "@/components/landing/PricingPlans";
 
 export default function Home() {
   return (
@@ -10,6 +12,8 @@ export default function Home() {
       <HeroLanding />
       <IdeaToBacklog />
       <AutomatedWorkflow />
+      <PricingPlans />
+      <BacklogCta />
     </>
   );
 }
